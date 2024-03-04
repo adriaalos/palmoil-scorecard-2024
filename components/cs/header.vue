9319372906b5bb93dc76679684e74b0f4a9fb885
adriaalos <adriaalos@adriaaloss-MacBook-Air.local>
2024-03-04 19:03:33 +0100

    cs - company

---
+++ components/cs/header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="u-page-header">
+        <div class="u-page-header__container u-container u-container--lg">
+            <div class="u-page-header__header">
+                <h1>CASE STUDY</h1>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+const tr = useTrStore().translations
+</script>
---
+++ pages/case-studies/[id]/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <cs-header />
+        <cs-company />
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const sc = useScStore()
+if (!sc.companies.length) await sc.fetchScores()
+await sc.setCompany(route.params.id)
+</script>
